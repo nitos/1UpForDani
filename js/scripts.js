5e9d29f9ee038419cdeaaa1248504a5624b8f1bc
Erian <edgarbb@gmail.com>
2017-05-22 12:36:08 -0600

    Adds correct instagram feed.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var feed = new Instafeed({
   get: 'user',
   resolution: 'low_resolution',
   accessToken: '511310839.25c76fb.73881ce9bd904c0bb4040697ec2ae234',
-  userId: '511310839',
+  userId: '5495440910',
   limit: 4,
   template: '<a class="instagram--galery-link" href="{{link}}"><img class="instagram--galery-photo" alt="{{caption}}" onload="instagramLoad(this)" src="{{image}}"></a>'
 });
