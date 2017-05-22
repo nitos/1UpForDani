9154b7756355914d1fc0ece66adbe21f38ae3491
Erian <edgarbb@gmail.com>
2017-05-22 04:13:55 -0600

    Small animation while image loads.

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,28 @@ var feed = new Instafeed({
   accessToken: '511310839.25c76fb.73881ce9bd904c0bb4040697ec2ae234',
   userId: '511310839',
   limit: 4,
-  template: '<a class="instagram--galery-link" href="{{link}}"><img class="instagram--galery-photo" alt="{{caption}}" src="{{image}}"></a>'
+  template: '<a class="instagram--galery-link" href="{{link}}"><img class="instagram--galery-photo" alt="{{caption}}" onload="instagramLoad(this)" src="{{image}}"></a>'
 });
 feed.run();
+
+function instagramLoad(img) {
+  wrapper = img.parentElement.parentElement;
+  total = wrapper.childElementCount,
+  img.parentElement.classList.add('loaded');
+  let loaded = 0;
+  for (var i = 0; i < total; i++) {
+    if (wrapper.childNodes[i].className === "instagram--galery-link loaded") {
+      loaded += 1;
+    }
+  }
+  if(loaded == total){
+    wrapper.parentElement.classList.add('loaded');
+  }
+};
+
+function imageLoad(img) {
+  img.classList.add('loaded');
+}
 
 window.onload = function() {
   document.getElementById('copy').addEventListener('click', function (e) {
